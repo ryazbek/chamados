5d078c2459f6ed7aedb046c54cf81660d3712447
ryazbek <136825526+ryazbek@users.noreply.github.com>
2023-07-31 13:38:12 -0300

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -1,32 +1,64 @@
 <?php
-  //Variáveis
-  $nome = $_POST['nome'];
-  $email = $_POST['email'];
-  $mensagem = $_POST['mensagem'];
-  $data_envio = date('d/m/Y');
-  $hora_envio = date('H:i:s');
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
-  //Compo E-mail
-  $arquivo = "
-    <html>
-      <p><b>Nome: </b>$nome</p>
-      <p><b>E-mail: </b>$email</p>
-      <p><b>Mensagem: </b>$mensagem</p>
-      <p>Este e-mail foi enviado em <b>$data_envio</b> às <b>$hora_envio</b></p>
-    </html>
-  ";
-  
-  //Emails para quem será enviado o formulário
-  $destino = "edipojoseoliveira@gmail.com";
-  $assunto = "Contato pelo Site";
+require 'autoload.php'
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
 
-  //Este sempre deverá existir para garantir a exibição correta dos caracteres
-  $headers  = "MIME-Version: 1.0\n";
-  $headers .= "Content-type: text/html; charset=iso-8859-1\n";
-  $headers .= "From: $nome <$email>";
+// Recolha os dados do formulário
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nome = $_POST["nome"];
+    $ramal = $_POST["ramal"];
+    $acesso_remoto = $_POST["acesso_remoto"];
+    $id = $_POST["id"];
+    $senha = $_POST["senha"];
+    $mensagem = $_POST["mensagem"];
 
-  //Enviar
-  mail($destino, $assunto, $arquivo, $headers);
-  
-  echo "<meta http-equiv='refresh' content='10;URL=../contato.html'>";
+    // Configurações do servidor SMTP do Yahoo
+    $smtpHost = 'smtp.mail.yahoo.com';
+    $smtpPort = 587;
+    $smtpUser = 'chamados.ryazbek';
+    $smtpPassword = 'jmtrkyutyqnljoyk';
+
+    // Configurações do email
+    $destinatario = 'informatica@ryazbek.com.br';
+    $assunto = 'Novo Chamado';
+
+    // Cria uma nova instância do PHPMailer
+    $mail = new PHPMailer(true);
+
+    try {
+        // Configuração do servidor SMTP
+        $mail->isSMTP();
+        $mail->Host = $smtpHost;
+        $mail->Port = $smtpPort;
+        $mail->SMTPAuth = true;
+        $mail->Username = $smtpUser;
+        $mail->Password = $smtpPassword;
+
+        // Configuração do email
+        $mail->setFrom('chamados.ryazbek@yahoo.com', 'Chamados');
+        $mail->addAddress('informatica@ryazbek.com.br');
+        $mail->Subject = 'Novo Chamado';
+
+        // Conteúdo do email
+        $conteudo_email = "Nome: $nome\n";
+        $conteudo_email .= "Ramal: $ramal\n";
+        $conteudo_email .= "Acesso remoto escolhido: $acesso_remoto\n";
+        $conteudo_email .= "ID: $id\n";
+        $conteudo_email .= "Senha: $senha\n\n";
+        $conteudo_email .= "Mensagem:\n$mensagem";
+
+        $mail->Body = $conteudo_email;
+
+        // Envia o email
+        $mail->send();
+        echo 'E-mail enviado com sucesso!';
+    } catch (Exception $e) {
+        echo 'Ocorreu um erro ao enviar o e-mail: ' . $mail->ErrorInfo;
+    }
+}
 ?>
