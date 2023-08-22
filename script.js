2bbc350627ac00c64268f042b746b39ff599ab62
ryazbek <136825526+ryazbek@users.noreply.github.com>
2023-08-22 15:31:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const emailInput = document.getElementById("email");
     const phoneInput = document.getElementById("phone");
     const addressInput = document.getElementById("address");
-    const signatureDiv = document.getElementById("signature");
+    const signatureTextarea = document.getElementById("signature");
 
     generateButton.addEventListener("click", function() {
         const name = nameInput.value;
@@ -15,11 +15,12 @@ document.addEventListener("DOMContentLoaded", function() {
         const address = addressInput.value;
 
         const signature = `
-            <span style="font-weight: bold; font-family: Arial; font-size: 11px;">${name}</span><br>
-            <span style="font-family: Arial; font-size: 10px;">${title}</span><br>
-            <span style="font-family: Arial; font-size: 8px; color: #0563C1;">E-mail: ${email}</span><br>
-            <span style="font-family: Arial; font-size: 8px; color: #767171;">Endereço da obra ou escritório: ${address}</span>
+            ${name}
+            ${title}
+            E-mail: ${email}
+            Telefone: ${phone}
+            Endereço da obra ou escritório: ${address}
         `;
-        signatureDiv.innerHTML = signature;
+        signatureTextarea.innerText = signature;
     });
 });
