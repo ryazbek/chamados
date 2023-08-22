d2234faf80e3f1c4ee3b27cbf24bd71e2394e7ff
ryazbek <136825526+ryazbek@users.noreply.github.com>
2023-08-22 15:19:38 -0300

    Add files via upload
    Criei um esquema de assinatura.

---
+++ script.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const generateButton = document.getElementById("generate");
+    const nameInput = document.getElementById("name");
+    const titleInput = document.getElementById("title");
+    const emailInput = document.getElementById("email");
+    const phoneInput = document.getElementById("phone");
+    const addressInput = document.getElementById("address");
+    const signatureTextarea = document.getElementById("signature");
+
+    generateButton.addEventListener("click", function() {
+        const name = nameInput.value;
+        const title = titleInput.value;
+        const email = emailInput.value;
+        const phone = phoneInput.value;
+        const address = addressInput.value;
+
+        const signature = `
+            <span style="font-weight: bold; font-family: Arial; font-size: 11px;">${name}</span><br>
+            <span style="font-family: Arial; font-size: 10px;">${title}</span><br>
+            <span style="font-family: Arial; font-size: 8px; color: #0563C1;">E-mail: ${email}</span><br>
+            <span style="font-family: Arial; font-size: 8px; color: #767171;">Endereço da obra ou escritório: ${address}</span>
+        `;
+        signatureTextarea.innerHTML = signature;
+    });
+});
