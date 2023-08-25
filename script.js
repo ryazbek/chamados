97add727cb513b783e1e70e0407f30ef150470fb
ryazbek <136825526+ryazbek@users.noreply.github.com>
2023-08-25 11:21:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const generateButton = document.getElementById("generate");
-    const nameInput = document.getElementById("name");
-    const titleInput = document.getElementById("title");
-    const emailInput = document.getElementById("email");
-    const phoneInput = document.getElementById("phone");
-    const addressInput = document.getElementById("address");
-    const signatureDiv = document.getElementById("signature");
+    // ... Aquisição de elementos do formulário e botão de geração ...
 
     generateButton.addEventListener("click", function() {
-        const name = nameInput.value;
-        const title = titleInput.value;
-        const email = emailInput.value;
-        const phone = phoneInput.value;
-        const address = addressInput.value;
+        // ... Aquisição de valores dos campos do formulário ...
 
         const signature = `
-            <br style="font-weight: bold; font-family: Arial; font-size: 18px;">${name}</br>
-            <br style="font-family: Arial; font-size: 15px;">${title}</br>
-            <br style="font-family: Arial; font-size: 10px; color: #0563C1;">E-mail: ${email}</br>
-            <br style="font-family: Arial; font-size: 10px; color: #767171;">Telefone: ${phone}</br>
-            <br style="font-family: Arial; font-size: 10px; color: #767171;">Endereço da obra ou escritório: ${address}</br>
+            <span style="font-weight: bold; font-family: Arial; font-size: 18px;">${name}</span>
+            <br style="font-family: Arial; font-size: 15px;">${title}
+            <br style="font-family: Arial; font-size: 10px; color: #0563C1;">E-mail: ${email}
+            <br style="font-family: Arial; font-size: 10px; color: #767171;">Telefone: ${phone}
+            <br style="font-family: Arial; font-size: 10px; color: #767171;">Endereço da obra ou escritório: ${address}
         `;
         signatureDiv.innerHTML = signature;
     });
